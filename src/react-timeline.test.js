13016a1c4d5e577fd811528a6a4b337da0129f17
Maximillian Fan Xavier <maximillianfx@gmail.com>
2020-08-22 13:29:14 -0300

    Add success test

--- src/react-timeline.test.js
+++ src/react-timeline.test.js
@@ -16,9 +16,21 @@ afterEach(() => {
     container = null;
 });
 
-it("Render with empty list", () => {
+it("Render incorrect list", () => {
     act(() => {
         render(<ReactTimeline list={[]}/>, container);
     });
     expect(container.textContent).toBe("Bad input");
+
+    act(() => {
+        render(<ReactTimeline list={[{key:1, text:"First", completed: false, actual:true},{key:2, text:"Second", completed: false, actual:true}]}/>, container);
+    });
+    expect(container.textContent).toBe("Bad input");
+});
+
+it("Render correct list", () => {
+    act(() => {
+        render(<ReactTimeline list={[{key:1, text:"First", completed: false, actual:true},{key:2, text:"Second", completed: false, actual:false}]}/>, container);
+    });
+    expect(container.textContent).toBe("FirstSecond");
 });
